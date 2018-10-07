a992f6fc7a528d1927ba2f09e880fd4f69d9f273
biabs1 <34272112+biabs1@users.noreply.github.com>
2018-10-07 15:31:46 -0300

    Create jogo.hs

---
+++ jogo_Haskell/jogo.hs
@@ -0,0 +1,14 @@
+{- PLP - 2018.2
+
+Jogo Categorias.
+
+Desenvolvedores: Alessandra Barros, Beatriz Souza, Luana Melo,
+Matheus Medeiros e Wener Wagner.
+-}
+
+mensagem_apresentacao :: IO()
+mensagem_apresentacao =
+    do putStrLn("Bem vind@ ao jogo Categorias!")
+
+main = do
+    mensagem_apresentacao
