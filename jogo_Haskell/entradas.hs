0aff073d40ab4c241ef737e05c9deb54b3e219e4
biabs1 <34272112+biabs1@users.noreply.github.com>
2018-10-16 14:11:49 -0300

    Update entradas.hs

--- jogo_Haskell/entradas.hs
+++ jogo_Haskell/entradas.hs
@@ -1,6 +1,26 @@
 module Entradas where
 
+import Data.List
+import System.IO
+
 import Mensagens
+
+-- Opcoes menu inicial
+opcao_invalida = -1
+novo_jogo = 1
+saida = 2
+
+-- Opcoes menu modos
+modo_nao_selecionado = -1
+modo_treino = 1
+modo_alternado = 2
+modo_classico = 3
+voltar = 4
+
+-- Numero de jogadores
+num_minimo_jogadores = 2 :: Int
+num_maximo_jogadores = 8 :: Int
+num_jogadores_invalido = -1 :: Int
 
 prompt :: IO()
 prompt = do putStr(">> ")
@@ -12,10 +32,10 @@ entrada_pegarOpcaoMenu =
     --o prompt não está sendo impresso na linha de leitura
     mensagem_menuPrincipal
     opcao <- readLn
-    if (opcao == 1)
+    if (opcao == novo_jogo)
       then putStrLn("Novo Jogo")
       else do
-        if (opcao == 2)
+        if (opcao == saida)
           then putStrLn("Saída")
           else do
             mensagem_opcaoInvalida
