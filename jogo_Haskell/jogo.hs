541e7b0fcabe5221b7699264026467fe2d711d35
biabs1 <34272112+biabs1@users.noreply.github.com>
2018-10-16 13:43:09 -0300

    Create jogo.hs

---
+++ jogo_Haskell/jogo.hs
@@ -0,0 +1,14 @@
+{- PLP - 2018.2
+
+Jogo Categorias.
+
+Desenvolvedores: Alessandra Barros, Beatriz Souza, Luana Melo,
+Matheus Medeiros e Wener Wagner.
+-}
+
+import Mensagens
+import Entradas
+
+main = do
+    mensagem_apresentacao
+    entrada_pegarOpcaoMenu
