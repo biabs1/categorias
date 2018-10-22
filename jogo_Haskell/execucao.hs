ae8bcc7da2789d5836925797e7b4fb35bec505df
biabs1 <34272112+biabs1@users.noreply.github.com>
2018-10-22 19:28:44 -0300

    Update execucao.hs

--- jogo_Haskell/execucao.hs
+++ jogo_Haskell/execucao.hs
@@ -1,6 +1,12 @@
 module Execucao where
 
 import Mensagens
+import Entradas
+
+-- Opcoes menu inicial
+opcao_invalida = -1
+novo_jogo = 1
+saida = 2
 
 -- Opcoes menu modos
 modo_nao_selecionado = -1
@@ -14,23 +20,13 @@ num_minimo_jogadores = 2
 num_maximo_jogadores = 8
 num_jogadores_invalido = -1
 
-loopEscolhaNumJogadores :: IO Int
-loopEscolhaNumJogadores =
-  do
-    mensagem_numJogadores
-    numParticipantes <- readLn
-    if (numParticipantes == num_jogadores_invalido)
-      then
-        do
-          mensagem_opcaoInvalida
-          loopEscolhaNumJogadores
-      else return(numParticipantes)
-
 modo_generico :: Int -> IO()
 modo_generico modo =
   do
     numParticipantes <- loopEscolhaNumJogadores
-    putStrLn(show numParticipantes)
+    mensagem_nomeJogadores
+  --  nomeSobrenomeJogadores <- entrada_receberNomeSobrenomeJogadores numParticipantes []
+  --  mensagem_jogadoresCadastrados numParticipantes nomeSobrenomeJogadores
 
 modo_treino_selecionado :: IO()
 modo_treino_selecionado =
@@ -61,4 +57,35 @@ loopEscolhaModoJogo =
           else do
             if (modo == modo_classico)
               then do modo_classico_selecionado
-              else mensagem_opcaoInvalida
+              else do
+                mensagem_opcaoInvalida
+                loopEscolhaModoJogo
+
+loopEscolhaNumJogadores :: IO Int
+loopEscolhaNumJogadores =
+  do
+    mensagem_numJogadores
+    numParticipantes <- readLn
+    if (numParticipantes >= num_minimo_jogadores && numParticipantes <= num_maximo_jogadores )
+      then return(numParticipantes)
+      else
+        do
+          mensagem_opcaoInvalida
+          loopEscolhaNumJogadores
+
+
+entrada_pegarOpcaoMenu :: IO()
+entrada_pegarOpcaoMenu =
+  do
+    --prompt
+    --o prompt não está sendo impresso na linha de leitura
+    mensagem_menuPrincipal
+    opcao <- readLn
+    if (opcao == novo_jogo)
+      then do loopEscolhaModoJogo
+      else do
+        if (opcao == saida)
+          then return()
+          else do
+            mensagem_opcaoInvalida
+            entrada_pegarOpcaoMenu
