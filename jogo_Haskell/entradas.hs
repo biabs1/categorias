889187a9597985b2073124fd8a5cff1a3b433473
biabs1 <34272112+biabs1@users.noreply.github.com>
2018-10-22 19:29:43 -0300

    Update entradas.hs

--- jogo_Haskell/entradas.hs
+++ jogo_Haskell/entradas.hs
@@ -4,41 +4,14 @@ import Data.List
 import System.IO
 
 import Mensagens
-import Execucao
-
--- Opcoes menu inicial
-opcao_invalida = -1
-novo_jogo = 1
-saida = 2
-
--- Opcoes menu modos
-modo_nao_selecionado = -1
-modo_treino = 1
-modo_alternado = 2
-modo_classico = 3
-voltar = 4
-
--- Numero de jogadores
-num_minimo_jogadores = 2
-num_maximo_jogadores = 8
-num_jogadores_invalido = -1
 
 prompt :: IO()
 prompt = do putStr(">> ")
 
-entrada_pegarOpcaoMenu :: IO()
-entrada_pegarOpcaoMenu =
-  do
-    --prompt
-    --o prompt não está sendo impresso na linha de leitura
-    mensagem_menuPrincipal
-    opcao <- readLn
-    if (opcao == novo_jogo)
-      then loopEscolhaModoJogo
-      else do
-        if (opcao == saida)
-          then putStrLn("Saída")
-          else do
-            mensagem_opcaoInvalida
-            entrada_pegarOpcaoMenu
-
+entrada_receberNomeSobrenomeJogadores :: Int -> [[Char]] -> [[Char]]
+entrada_receberNomeSobrenomeJogadores numParticipantes nomeSobrenomeJogadores =
+  if numParticipantes == 0
+    then nomeSobrenomeJogadores
+    else do
+    --  nomeSobrenome <- readLn
+      entrada_receberNomeSobrenomeJogadores (numParticipantes - 1) (nomeSobrenomeJogadores)
