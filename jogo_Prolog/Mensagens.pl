ce7c5f58c682e80f5506a41e6010aa8021d8a36d
Matheus Silva Medeiros <33933832+Matheusrsm@users.noreply.github.com>
2018-11-20 18:20:01 -0300

    Update Mensagens.pl

--- jogo_Prolog/Mensagens.pl
+++ jogo_Prolog/Mensagens.pl
@@ -1,121 +1,84 @@
-% Implementação das mensagens
+% Mensagens do Jogo
 
 cabecalho:-
-    write("========== JOGO CATEGORIAS =========="),nl,nl.
-
+    writeln("========== JOGO CATEGORIAS =========="),nl.
 
 mensagem_apresentacao:-
-    clear(),
-    cabecalho,
-    write("Bem vind@ ao jogo Categorias!"),nl,
+    tty_clear, cabecalho,
+    writeln("Bem vind@ ao jogo Categorias!"),
     sleep(2),
-    write("Você testará o quanto de palavras você sabe ou consegue se lembrar de algumas categorias."),
+    writeln("Você testará o quanto de palavras você sabe ou consegue se lembrar de algumas categorias."),
     sleep(3).
 
 mensagem_menuPrincipal:-
-    clear(),
-    cabecalho,
-    write("Você deseja começar um novo jogo?"),nl,
-    write("1 - Novo Jogo"),nl,
-    write("2 - Sair"),nl.
+    tty_clear, cabecalho,
+    writeln("Você deseja começar um novo jogo?"), writeln("1 - Novo Jogo"), writeln("2 - Sair").
 
 mensagem_opcaoInvalida:-
-	write("** Opção inválida! **"),nl,
-	write("** Reveja as opções possíveis. **"),nl,
+    writeln("** Opção inválida! **"), writeln("** Reveja as opções possíveis. **"),
     sleep(3).
 
-
 mensagem_modosJogo:-
-	clear(),
-	cabecalho,
-	write("Digite o número correspondente ao modo que você quer"),nl,
-	write("1 - Modo Treino"),nl,
-	write("2 - Modo Alternado"),nl,
-	write("3 - Modo Clássico"),nl,
-	write("4 - Voltar").
-
-
-mensagem_modoClassicoSelecionado:-
-	clear(),
-	cabecalho,
-	write("Modo Clássico selecionado!"),nl,
-	sleep(2).
-
-
-mensagem_modoAlternadoSelecionado:-
-	clear(),
-	cabecalho,
-	write("Modo Alternado selecionado!"),nl,
-	sleep(2).
-
+    tty_clear, cabecalho,
+    writeln("Digite o número correspondente ao modo que você quer"), 
+    writeln("1 - Modo Treino"), writeln("2 - Modo Alternado"), writeln("3 - Modo Clássico"), writeln("4 - Voltar").
 
 mensagem_modoTreinoSelecionado:-
-	clear(),
-	cabecalho,
-	write("Modo Treino selecionado!"),nl,
-	sleep(2),
-	write("Cuidado para não ser derrotado(a) pelo(s) bot(s)!"),nl,
-	sleep(3).
+    tty_clear, cabecalho,
+    writeln("Modo Treino selecionado!"),
+    sleep(2),
+    writeln("Cuidado para não ser derrotado(a) pelo(s) bot(s)!"),
+    sleep(3).
 
+mensagem_modoAlternadoSelecionado:-
+    tty_clear, cabecalho,
+    writeln("Modo Alternado selecionado!"),
+    sleep(2).
+    
+mensagem_modoClassicoSelecionado:-
+    tty_clear, cabecalho,
+    writeln("Modo Clássico selecionado!"),
+    sleep(2).
 
 mensagem_numJogadores:-
-	clear(),
-	cabecalho,
-	write("Digite a quantidade de jogadores"),
-	write("Este valor deve estar entre 2 e 8 inclusive:"),nl.
-
+    tty_clear, cabecalho,
+    writeln("Digite a quantidade de jogadores"), writeln("Este valor deve estar entre 2 e 8 inclusive:").
 
 mensagem_nomeJogadores:-
-	clear(),
-	cabecalho,
-	write("Digite o nome e sobrenome do(s) jogador(es) humano(s)"),nl,
-	write("Um jogador por linha:"),nl.
-
+    tty_clear, cabecalho,
+    writeln("Digite o nome e sobrenome do(s) jogador(es) humano(s)"), writeln("Um jogador por linha:").
 
 mensagem_categoriaSorteada(categoriaSorteada):-
-	clear(),
-	cabecalho,
-    write("Categoria sorteada: "),write(categoriaSorteada),nl,nl,
-	write("Cada jogador deverá informar um ítem pertencente à categoria sorteada."),nl,
-	write("Caso não se recorde de algum item, digite #"),nl,
-	sleep(5).
+    tty_clear, cabecalho,
+    write("Categoria sorteada: "), writeln(categoriaSorteada), nl,
+    writeln("Cada jogador deverá informar um ítem pertencente à categoria sorteada."), 
+    writeln("Caso não se recorde de algum item, digite #"),
+    sleep(5).
 
-
-mensagem_informarPalavraCategoria(categoriaSorteada, nomeJogador, sobrenomeJogador):-
-	clear(),
-	cabecalho,
-    write("Categoria: "),write(categoriaSorteada),nl,write("Vez do jogador(a): "),write(nomeJogador),write(" "),write(sobrenomeJogador),nl,nl,
-	write("Informe um ítem desta categoria (sem acentos): "),nl.
+mensagem_informarPalavraCategoria(categoriaSorteada, nomeJogador):-
+    tty_clear, cabecalho,
+    write("Categoria: "), writeln(categoriaSorteada), write("Vez do jogador: "), writeln(nomeJogador),
+    writeln("Informe um ítem desta categoria (sem acentos): "),
 
 mensagem_itemAceito:-
-    nl,
-	write("Item aceito!"),nl,
-	sleep(1).
+    nl, writeln("Item aceito!"),
+    sleep(1).
 
 mensagem_palavraNaoCadastrada:-
-    nl,
-	write("ATENÇÃO! Esta palavra não está cadastrada!"),nl,nl,
-	write("Este ítem pertence mesmo a esta categoria? s/n"),nl.
+    nl, writeln("ATENÇÃO! Esta palavra não está cadastrada!"), nl,
+    writeln("Este ítem pertence mesmo a esta categoria? s/n").
 
+mensagem_vencedor(nomeJogador):-
+    tty_clear, cabecalho,
+    write("O vencedor da vez é: "), write(nomeJogador), writeln(". Parabéns!"),
+    sleep(4).
 
-mensagem_vencedor(nomeJogador, sobrenomeJogador):-
-	clear(),
-	cabecalho,
-    write("O vencedor da vez é: "),write(nomeJogador),write(" "),write(sobrenomeJogador),write(". Parabéns!"),nl,nl,
-	sleep(4).
-
-
-mensagem_perdedor(nomeJogador, sobrenomeJogador):-
-    clear(),
-	cabecalho,
-    write("Que pena, "),write(nomeJogador),write(" "),write(sobrenomeJogador),write(" está fora do jogo!"),nl,
-	sleep(4).
+mensagem_perdedor(nomeJogador):-
+    tty_clear, cabecalho,
+    write("Que pena, "), write(nomeJogador), writeln(" está fora do jogo!"),
+    sleep(4).
 
 mensagem_modoAlternadoSorteio(categoriaSorteada):-
-	clear(),
-	cabecalho,
-    write("Modo Alternado"),nl,
-    write("Uma nova categoria foi sorteada:"),
-    write(categoriaSorteada),nl,
-	sleep(3).
-
+    tty_clear, cabecalho,
+    writeln("Modo Alternado"), write("Uma nova categoria foi sorteada: "), writeln(categoriaSorteada),
+    sleep(3).
