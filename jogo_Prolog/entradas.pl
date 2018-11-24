0919bff21a2b367b0d4628a70059a5b1f271a1f2
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2018-11-24 11:12:36 -0300

    recebendo nome e sobrenome dos jogadores

--- jogo_Prolog/entradas.pl
+++ jogo_Prolog/entradas.pl
@@ -12,12 +12,12 @@ receberString(String):- read_line_to_string(user_input, String).
 
 entrada_pegarOpcaoMenu(Opcao):- mensagem_menuPrincipal, imprimePrompt, receberNumero(Opcao).
 
-entrada_receberNomeSobrenomeJogadores(NumParticipantes,_, NomeSobrenomeJogadoresFinal):-
+entrada_receberNomeSobrenomeJogadores(NumParticipantes, NomeSobrenomeJogadores, NomeSobrenomeJogadoresFinal):-
     NumParticipantes =:= 0,
-    NomeSobrenomeJogadoresFinal = NomeSobrenomeJogadoresFinal.
+    NomeSobrenomeJogadoresFinal = NomeSobrenomeJogadores.
 
-entrada_receberNomeSobrenomeJogadores(NumParticipantes, NomeSobrenomeJogadores,NomeSobrenomeJogadoresFinal):-
+entrada_receberNomeSobrenomeJogadores(NumParticipantes, NomeSobrenomeJogadores, NomeSobrenomeJogadoresFinal):-
     write("Digite seu nome e sobrenome: "),
     receberString(NomeSobrenome),
-    adicionarElemento(NomeSobrenomeJogadores, NomeSobrenome, NomeSobrenomeJogadoresFinal),
-    entrada_receberNomeSobrenomeJogadores(NumParticipantes - 1,  NomeSobrenomeJogadores, NomeSobrenomeJogadoresFinal).
+    concatenaListas(NomeSobrenomeJogadores, [NomeSobrenome], NovoNomeSobrenomeJogadores),
+    entrada_receberNomeSobrenomeJogadores(NumParticipantes - 1,  NovoNomeSobrenomeJogadores, NomeSobrenomeJogadoresFinal).
